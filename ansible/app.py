380b11c297e6724f4be21cb30062e69dc59d451e
Neslihan Bükte <neslihanbukte20@gmail.com>
2025-01-13 00:00:38 +0300

    Flask code was written

--- ansible/app.py
+++ ansible/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! Vagrant ve Ansible ile deploy edildi."
+    return "Hello, World! Uygulama Vagrant ve Ansible ile deploy edildi."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
